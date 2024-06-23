3cb562197bc42347d3b7d0f94f4a5fa8476ec814
Your Name <you@example.com>
2024-06-23 23:07:12 +0530

    first commit

---
+++ backend/models/Task.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  title:{
+    type:String,
+    required:true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  dueDate:{
+    type:Date,
+    required:true,
+  }
+}, {
+  timestamps: true
+});
+
+
+const Task = mongoose.model("Task", taskSchema);
+module.exports = Task;
